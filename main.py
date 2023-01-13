9e033d3c2b96a33d46f8021873438675703abc18
suhani013 <122560290+suhani013@users.noreply.github.com>
2023-01-13 13:13:32 -0500

    Update main.blocks, main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+def on_button_pressed_a():
+    basic.show_icon(IconNames.HEART)
+input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def on_button_pressed_b():
+    basic.show_icon(IconNames.SMALL_HEART)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+basic.show_icon(IconNames.CHESSBOARD)
