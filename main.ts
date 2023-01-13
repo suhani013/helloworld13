251dbff3f9d15f9bd7ab3dc07df0a5ab105140a3
suhani013 <122560290+suhani013@users.noreply.github.com>
2023-01-13 13:11:59 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.SmallHeart)
+})
+basic.showIcon(IconNames.Chessboard)
